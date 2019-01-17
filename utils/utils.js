5938e6760e18136bf4787851d09090de98c896b1
Antonia <lantoniagherghel@gmail.com>
2019-01-17 23:28:47 +0200

    comment

--- utils/utils.js
+++ utils/utils.js
@@ -49,27 +49,43 @@ function Login(){
 {
 	document.getElementById("ProjNamm").innerHTML = "Project name: "+localStorage.getItem("projectName")+"<br>The User logged in is "+localStorage.getItem("user");
 }());
+
+
+function SaveButton(){
+var modal = document.getElementById('altModal');//pop-up new
+var btn = document.getElementById("savebtn");// Get the button that opens the modal
+var span = document.getElementsByClassName("close")[0];// Get the <span> element that closes the modal
+btn.onclick = function() {// When the user clicks on the button, open the modal
+  modal.style.display = "block";
+}
+console.log(btn);
+span.onclick = function() {// When the user clicks on <span> (x), close the modal
+  modal.style.display = "none";
+}
+window.onclick = function(event) {// When the user clicks anywhere outside of the modal, close it
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+
+
 function showCreateSprint(){
 //pop-up for new sprint
 var modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 var btn = document.getElementById("show-create");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 console.log(btn);
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
